dce651625bba23d6e61278240d69101075a72d1f
Nathanael Burt <nathanael.burt@gmail.com>
2014-03-11 17:46:09 -0600

    is a leap year if divisible by 4

---
+++ lib/leap_year_calculator.rb
@@ -0,0 +1,7 @@
+class LeapYearCalculator
+  def yes?(year)
+    if year % 4 == 0
+      true
+    end
+  end
+end
---
+++ spec/leap_year_calculator_spec.rb
@@ -0,0 +1,20 @@
+require 'rspec/core'
+require 'leap_year_calculator'
+
+describe LeapYearCalculator do
+  it "is a leap year if it is divisible by 4" do
+    leap_year_calculator = LeapYearCalculator.new
+
+    expected = true
+
+    expect(leap_year_calculator.yes?(2012)).to eq expected
+  end
+end
+
+
+# 1) The class is not initialized with anything special.
+# You will call a method called yes? that will tell you if it is a leap year or not.
+# 2) The year is a leap year if it is divisible by 4. (2001 is not a leap year but 1996 is a leap year)
+# 3) The year is leap year if it is divisible by 100 and divisible by 400.
+# If it is only divisible by 100, it is not a leap year.
+# (1900 is not a leap year but 2000 is a leap year)
