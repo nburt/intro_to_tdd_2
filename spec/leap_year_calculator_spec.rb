fdd1a5b00dedf0c2f0a00501f49a135372773a2f
Nathanael Burt <nathanael.burt@gmail.com>
2014-03-11 18:04:19 -0600

    Changed test to check if a year is not a leap year

--- spec/leap_year_calculator_spec.rb
+++ spec/leap_year_calculator_spec.rb
@@ -5,9 +5,9 @@ describe LeapYearCalculator do
   it "is a leap year if it is divisible by 4" do
     leap_year_calculator = LeapYearCalculator.new
 
-    expected = true
+    expected = false
 
-    expect(leap_year_calculator.yes?(2012)).to eq expected
+    expect(leap_year_calculator.yes?(2005)).to eq expected
   end
 end
 
